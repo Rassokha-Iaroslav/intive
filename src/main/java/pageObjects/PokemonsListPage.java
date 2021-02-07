c60d556e2f5521dc85c068595a4c86c45c71263f
Iaroslav Rassokha <rassochaja88@gmail.com>
2021-02-07 15:37:54 +0100

    refactoring

--- src/main/java/pageObjects/PokemonsListPage.java
+++ src/main/java/pageObjects/PokemonsListPage.java
@@ -3,8 +3,6 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.ArrayList;
@@ -13,12 +11,7 @@ import java.util.List;
 public class PokemonsListPage extends BasePage {
     private WebDriver driver;
 
-    private static final String Y_LIST_OF_POKEMONS = "table.roundy:nth-child(102) > tbody:nth-child(1) > tr > td:nth-child(3) > a";
-
     private static String templateForListOfPockemonsOnLetterSelector = "//div[@id='mw-content-text']/h3/span[@id=\"letterPlaceholder\"]/../following-sibling::table[1]/tbody/tr/td[2]/a";
-
-    @FindBy(how = How.CSS, using = Y_LIST_OF_POKEMONS)
-    public List<WebElement> pokemonsOnLetterY;
 
     public PokemonsListPage(WebDriver driver) {
         super(driver);
